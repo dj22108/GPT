0472a93e1d3950f6e46db2d6c4707ac002098594
Dhananjay <46827345+dj22108@users.noreply.github.com>
2023-05-18 10:42:17 +0530

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -1,9 +1,11 @@
 import gpt_2_simple as gpt2
 import os
+import shutil
+from tqdm import tqdm
 
 # Specify the path to the downloaded GPT-2 model checkpoint
-model_name = "124M"
-model_checkpoint_dir = "models/124M"
+model_name = "355M"
+model_checkpoint_dir = "models/355M"
 
 # Download the GPT-2 model if it's not already downloaded
 if not os.path.exists(model_checkpoint_dir):
@@ -13,14 +15,29 @@ if not os.path.exists(model_checkpoint_dir):
 data_file = "employee_data.txt"
 
 # Specify the path to the output folder
-output_folder = "output/"
+output_folder = "output"
 
 # Create the output folder if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
 
-# Fine-tune the GPT-2 model
+# Fine-tune the GPT-2 model with progress bar
 sess = gpt2.start_tf_sess()
-gpt2.finetune(sess, data_file, model_name=model_name, checkpoint_dir=model_checkpoint_dir, model_dir=output_folder)
+
+# Define the progress bar
+total_iterations = 5
+progress_bar = tqdm(total=total_iterations, unit='iteration')
+
+# Fine-tuning loop
+for _ in range(total_iterations):
+    gpt2.finetune(sess, data_file, model_name=model_name, checkpoint_dir=model_checkpoint_dir,learning_rate=0.0001,
+                  batch_size=4, steps=4)
+    progress_bar.update(1)
 
 # Save the fine-tuned model
+checkpoint_path = os.path.join(output_folder, "checkpoint")
+os.makedirs(checkpoint_path, exist_ok=True)
+shutil.copyfile(os.path.join(model_checkpoint_dir, "hparams.json"), os.path.join(checkpoint_path, "hparams.json"))
 gpt2.save_checkpoint(output_folder)
+
+# Close the progress bar
+progress_bar.close()
